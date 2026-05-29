b27e8108ecd1a1912a42e5e492eb0d4b1cba2e28
Paras <paras@BOOK-2OEO6GTEEJ.none>
2026-05-29 20:44:58 +0530

    Automated update: 2026-05-29 20:44

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,7 @@ import { defineConfig } from 'astro/config';
 import preact from '@astrojs/preact';
 // https://astro.build/config
 export default defineConfig({
-  integrations: [preact()]
+  site: 'https://paraschausali48-lab.github.io',
+  base: '/Paras-Eureka-Forbes',
+  integrations: [preact()],
 });
