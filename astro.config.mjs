1caf66cbe22205c018a4ea6a07f91f1822a28eff
Paras <paras@BOOK-2OEO6GTEEJ.none>
2026-05-30 15:03:09 +0530

    Automated update: 2026-05-30 15:03

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 import preact from '@astrojs/preact';
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://paraschausali48-lab.github.io',
+  site: 'https://paraschausali48-lab.github.io/Paras-Eureka-Forbes/',
   base: '/Paras-Eureka-Forbes/',
   trailingSlash: 'always',
   prefetch: true,
